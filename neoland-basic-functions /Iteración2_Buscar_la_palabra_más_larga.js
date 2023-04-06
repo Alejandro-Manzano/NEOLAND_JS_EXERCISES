435aab9c3012cef50d6b12fed469172e10dbfd75
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2023-04-06 13:52:41 +0200

    Iteración #2: Buscar la palabra más larga

---
+++ neoland-basic-functions /Iteración2_Buscar_la_palabra_más_larga.js
@@ -0,0 +1,24 @@
+/* Completa la función que tomando un array de strings como argumento devuelva el más largo, 
+
+en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+
+Puedes usar este array para probar tu función: */
+
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+
+function findLongestWord(param) {
+    
+    let avengerBig = "";
+
+  for(let i=0; i<avengers.length; i++){
+
+    if(avengers[i].length > avengerBig.length){
+        avengerBig = avengers[i];
+    }
+
+}
+return(avengerBig);
+}
+
+console.log(findLongestWord(avengers));
+
