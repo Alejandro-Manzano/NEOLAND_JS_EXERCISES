fc8109659af6350dbfa87260f5fa4d4484eea44d
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2023-04-06 14:39:55 +0200

    Iteración #5: Calcular promedio de strings

---
+++ neoland-basic-functions /Iteración5_Calcular_promedio_de_strings.js
@@ -0,0 +1,28 @@
+/* Crea una función que reciba por parámetro un array y cuando es un valor number 
+
+lo sume y de lo contrario cuente la longitud del string y lo sume. 
+
+Puedes usar este array para probar tu función: */
+
+const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
+
+function averageWord(param) {
+  let sumaNumber = 0;
+  
+
+  for (let i=0; i<mixedElements.length; i++){
+
+   /*  if (typeof(mixedElements[i]) === "number"){
+        sumaNumber += mixedElements[i];
+    }
+    else sumaNumber += mixedElements[i].length; */
+
+    (typeof(mixedElements[i]) === "number") ? sumaNumber += mixedElements[i] : sumaNumber += mixedElements[i].length;
+
+  }
+  return sumaNumber;
+
+  
+}
+
+console.log(averageWord());
