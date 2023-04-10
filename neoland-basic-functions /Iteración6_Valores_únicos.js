771a817288ab3266b5d6247687dcbdb205f505fe
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2023-04-10 17:15:01 +0200

    Iteración #6: Valores únicos

---
+++ neoland-basic-functions /Iteración6_Valores_únicos.js
@@ -0,0 +1,34 @@
+/* Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados,
+
+en caso que existan los elimina para retornar un array sin los elementos duplicados.
+ 
+Puedes usar este array para probar tu función: */
+
+const duplicates = [
+    'sushi',
+    'pizza',
+    'burger',
+    'potatoe',
+    'pasta',
+    'ice-cream',
+    'pizza',
+    'chicken',
+    'onion rings',
+    'pasta',
+    'soda'
+];
+
+
+function removeDuplicates(param) {
+    let dontRepeat = [];
+
+    for(i=0; i<param.length; i++){
+
+        !dontRepeat.includes(param[i]) ? dontRepeat.push(param[i]) : null;
+    }
+
+    return dontRepeat;
+
+  }
+  
+  console.log(removeDuplicates(duplicates))
