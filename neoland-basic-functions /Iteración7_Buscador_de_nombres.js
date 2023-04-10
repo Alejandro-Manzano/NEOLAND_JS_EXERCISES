6b5672b9f6a260407e16b21128d8914b3cd1f843
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2023-04-10 18:04:39 +0200

    Iteration #8: Contador de repeticiones

---
+++ neoland-basic-functions /Iteración7_Buscador_de_nombres.js
@@ -0,0 +1,32 @@
+/* Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array 
+- comprueba si existe el elemento, en caso que existan nos devuelve un true y la posición de dicho elemento y por la contra un false. 
+Puedes usar este array para probar tu función: */
+
+const nameFinder = [
+    'Peter',
+    'Steve',
+    'Tony',
+    'Natasha',
+    'Clint',
+    'Logan',
+    'Xabier',
+    'Bruce',
+    'Peggy',
+    'Jessica',
+    'Marc'
+  ];
+
+  const finderName = (array, value) => {
+    
+    for (let i = 0; i < array.length; i++) {
+
+      if (array[i] === value) {
+        return {exists: true, index: i};
+      }
+      
+    }
+    return "false"
+  }
+  
+  console.log(finderName(nameFinder, "Bruce"));
+
