ff940d729942162046b597675a2a997807b326fd
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2023-04-11 14:35:45 +0200

    Iteración #2: Condicionales avanzados

---
+++ neoland-loops/Iteración2_Condicionales_avanzados.js
@@ -0,0 +1,32 @@
+// Comprueba en cada uno de los usuarios que tenga al menos dos trimestres aprobados 
+
+// y añade la propiedad isApproved a true o false en consecuencia. 
+
+// Una vez lo tengas compruébalo con un console.log. 
+
+const alumns = [
+
+        {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
+		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+
+];
+
+alumns.forEach((alumn) => {
+
+    if((alumn.T1 && alumn.T2 == true)||(alumn.T1 && alumn.T3 == true)||(alumn.T3 && alumn.T2 == true)) {
+
+        alumn.isApproved = true;
+        console.log(alumn.name + " ha aprobado");
+
+    }else 
+
+        alumn.isApproved = false;
+        console.log(alumn.name+ " no ha aprobado");
+
+});
+
+console.log(alumns);
+
