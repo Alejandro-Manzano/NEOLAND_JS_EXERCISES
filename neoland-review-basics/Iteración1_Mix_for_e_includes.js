d7f5916840e78c430e15f830992c584468ac5619
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2023-04-12 09:42:40 +0200

    Iteración #1: Mix for e includes

---
+++ neoland-review-basics/Iteración1_Mix_for_e_includes.js
@@ -0,0 +1,32 @@
+/* Dado el siguiente javascript usa forof para recorrer el array de películas, 
+genera un nuevo array con las categorías de las películas e imprime por consola el array de categorías. 
+Ten en cuenta que las categorías no deberían repetirse. 
+Para filtrar las categorías puedes ayudarte de la función .includes() */
+
+const movies = [
+    {title: 'Madaraspar', duration: 192, categories: ['comedia', 'aventura']},
+    {title: 'Spiderpan', duration: 122, categories: ['aventura', 'acción']},
+    {title: 'Solo en Whatsapp', duration: 223, categories: ['comedia', 'thriller']},
+    {title: 'El gato con guantes', duration: 111, categories: ['comedia', 'aventura', 'animación']},
+    
+]
+
+const moviesFunction = (Array) => {
+
+    let dontRepeat=[];
+
+    for (element of movies){
+
+        for(categories of element.categories){ 
+        
+            (!dontRepeat.includes(categories)) && dontRepeat.push(categories)   
+        
+        } 
+    }
+
+    return(dontRepeat);
+
+}
+
+console.log(moviesFunction(movies));
+
