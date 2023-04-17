4408b9c389f5f756cdea0f476beb5562d465ebdc
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2023-04-17 18:05:38 +0200

    Iteracion 3

--- neoland-basic-functions /Iteración3_Calcular_la_suma.js
+++ neoland-basic-functions /Iteración3_Calcular_la_suma.js
@@ -9,7 +9,7 @@ const numbers = [1, 2, 3, 5, 45, 37, 58];
 function sumAll(param) {
 
     let sum = 0;
-  for (i=0; i<numbers.length; i++){
+  for (i=0; i<param.length; i++){
     
     sum += numbers[i];
   }
